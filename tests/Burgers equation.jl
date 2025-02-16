d4a7950ebcc3150c4c30a803526e9116a583fb4d
JohannesMorkrid <johannes.morkrid@gmail.com>
2025-02-16 17:48:17 +0100

    Added a scheme test

--- tests/Burgers equation.jl
+++ tests/Burgers equation.jl
@@ -28,7 +28,7 @@ t_b = -1 / (minimum(real(domain.transform.iFT * dudy)))
 t_span = [0, 0.9 * t_b]
 
 # Initialize problem
-prob = SpectralODEProblem(L, N, domain, u0, t_span, p=parameters, dt=0.001)
+prob = SpectralODEProblem(L, N, domain, u0, t_span, p=parameters, dt=0.0001)
 
 # Initialize output
 output = Output(prob, 1000, [BurgerCFLDiagnostic(10), ProgressDiagnostic(10)])
@@ -51,18 +51,18 @@ plot!(timesteps, convergence3, xaxis=:log, yaxis=:log, label="MSS3", color="oran
 savefig("Timestep convergence, Burgers equation (N =$(domain.Ny)).pdf")
 
 ## Resolution convergence test
-resolutions = [2, 4, 8, 16, 32, 64, 128]#, 256]#, 512, 1024] #[2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]
-_, convergence1 = test_resolution_convergence(prob, gaussian, HeatEquationAnalyticalSolution2, resolutions, MSS1())
-_, convergence2 = test_resolution_convergence(prob, gaussian, HeatEquationAnalyticalSolution2, resolutions, MSS2())
-_, convergence3 = test_resolution_convergence(prob, gaussian, HeatEquationAnalyticalSolution2, resolutions, MSS3())
+resolutions = [2, 4, 8, 16, 32, 64, 128, 256]#, 512, 1024] #[2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]
+_, convergence1 = test_resolution_convergence(prob, gaussian, burgers_equation_analytical_solution, resolutions, MSS1())
+_, convergence2 = test_resolution_convergence(prob, gaussian, burgers_equation_analytical_solution, resolutions, MSS2())
+_, convergence3 = test_resolution_convergence(prob, gaussian, burgers_equation_analytical_solution, resolutions, MSS3())
 
 plot(resolutions, convergence1, xaxis=:log2, yaxis=:log, label="MSS1")
 plot!(resolutions, convergence2, xaxis=:log2, yaxis=:log, label="MSS2", color="dark green")
 plot!(resolutions, convergence3, xaxis=:log2, yaxis=:log, label="MSS3", color="orange")
 plot!(resolutions[1:end-4], 0.5 * exp.(-0.5 * resolutions)[1:end-4], label=L"\frac{1}{2}\exp\left(-\frac{N}{2}\right)", linestyle=:dash,
     xaxis=:log2, yaxis=:log, xticks=resolutions, xlabel=L"N_x \wedge N_y",
-    ylabel=L"||U-u_a||/N_xN_y", title="Resolution convergence, Lin-Diffusion (dt=$(prob.dt))")
-savefig("Resolution convergence, Lin-Diffusion (dt=$(prob.dt)).pdf")
+    ylabel=L"||U-u_a||/N_xN_y", title="Resolution convergence, Burgers equation (dt=$(prob.dt))")
+savefig("Resolution convergence, Burgers equation (dt=$(prob.dt)).pdf")
 
 
 
