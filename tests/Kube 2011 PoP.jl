7db23cfdfc797b3bab30f91eceea5451a36674c9
JohannesMorkrid <johannes.morkrid@gmail.com>
2025-02-21 16:04:58 +0100

    Some initial experimenting, but will go over to focus on storing the output now, as it is the biggest non-apparent botle neck in my workflow

--- tests/Kube 2011 PoP.jl
+++ tests/Kube 2011 PoP.jl
@@ -2,9 +2,8 @@
 include("../src/HasagawaWakatini.jl")
 
 ## Run scheme test for Burgers equation
-#domain = Domain(512, 512, 200, 100, anti_aliased=false)
-domain = Domain(256, 256, 50, 50, anti_aliased=false)
-#domain = Domain(1024, 1024, 50, 50, anti_aliased=false)
+domain = Domain(1024, 1024, 50, 50, anti_aliased=false)
+#domain = Domain(256, 256, 50, 50, anti_aliased=false)
 u0 = gaussian.(domain.x', domain.y, A=0.1, B=1, l=1)
 
 # Linear operator
@@ -16,12 +15,12 @@ end
 
 # Non-linear operator
 function N(u, d, p, t)
-    η = u[:, :, 1]
-    Ω = u[:, :, 2]
+    η = @view u[:, :, 1]
+    Ω = @view u[:, :, 2]
     ϕ = solvePhi(Ω, d)
     dη = -poissonBracket(ϕ, η, d)
-    dη -= p["kappa"]*quadraticTerm(diffX(η, d), diffX(η, d), d)
-    dη -= p["kappa"]*quadraticTerm(diffY(η, d), diffY(η, d), d)
+    dη += p["kappa"]*quadraticTerm(diffX(η, d), diffX(η, d), d)
+    dη += p["kappa"]*quadraticTerm(diffY(η, d), diffY(η, d), d)
     dΩ = -poissonBracket(ϕ, Ω, d)
     dΩ -= diffY(η, d)
     return [dη;;; dΩ]
@@ -36,46 +35,64 @@ parameters = Dict(
 # Time interval
 t_span = [0, 20]
 
+# Speed up simulation
+FFTW.set_num_threads(16)
+
 # The problem
 prob = SpectralODEProblem(L, N, domain, [u0;;; zero(u0)], t_span, p=parameters, dt=1e-3)
 
 # Array of diagnostics want
 diagnostics = [
-    ProbeDensityDiagnostic([(5, 0), (8.5, 0), (11.25, 0), (14.375, 0)], N=10),
+    #ProbeDensityDiagnostic([(5, 0), (8.5, 0), (11.25, 0), (14.375, 0)], N=10),
     RadialCOMDiagnostic(),
     ProgressDiagnostic(100),
-    CFLDiagnostic(),
-    RadialCFLDiagnostic(100),
-    PlotDensityDiagnostic(100)
+    #CFLDiagnostic(),
+    PlotDensityDiagnostic(1000)
 ]
 
+# The problem
+prob = SpectralODEProblem(L, N, domain, [u0;;; zero(u0)], t_span, p=parameters, dt=1e-3)
+
 # The output
-output = Output(prob, 1, diagnostics) #progressDiagnostic
+output = Output(prob, 21, diagnostics)
 
 ## Solve and plot
 sol = spectral_solve(prob, MSS3(), output)
 
-## Recreate Garcia et al. plots
-display(heatmap(sol.u[5][:, :, 1], levels=10, aspect_ratio=:equal))
-display(heatmap(sol.u[10][:, :, 1], levels=10, aspect_ratio=:equal))
-display(heatmap(sol.u[15][:, :, 1], levels=10, aspect_ratio=:equal))
-display(heatmap(sol.u[end][:, :, 1], levels=10, aspect_ratio=:equal))
-display(heatmap(sol.u[5][:, :, 2], levels=10, aspect_ratio=:equal, color=:jet))
-display(heatmap(sol.u[10][:, :, 2], levels=10, aspect_ratio=:equal, color=:jet))
-display(heatmap(sol.u[15][:, :, 2], levels=10, aspect_ratio=:equal, color=:jet))
-display(heatmap(sol.u[end][:, :, 2], levels=10, aspect_ratio=:equal, color=:jet))
+# Folder path
+cd("tests/Garcia 2005 Pop/")
 
-## Save data
-using JLD
-save("reproduced data Kube 2011 PoP.jld", "data", sol.u)
+## Recreate max velocity plot
+tends = logspace(2, -2, 22)
+dts = tends / 2000
+amplitudes = logspace(-2, 5, 22)
+max_velocities = similar(amplitudes)
+velocities = Vector{typeof(data)}(undef, length(amplitudes))
 
-## Analyze data
-data = zeros(length(output.diagnostics[1].data))
-for i in eachindex(data)
-    data[i] = sol.diagnostics[1].data[i][2]
+parameters = Dict(
+    "nu" => 1e-3,
+    "kappa" => 1e-3
+)
+
+for (i, A) in enumerate(amplitudes)
+    # Update initial initial_condition
+    u0 = gaussian.(domain.x', domain.y, A=A, B=0, l=1)
+    # Update problem 
+    prob = SpectralODEProblem(L, N, domain, [u0;;; zero(u0)], [0, tends[i]], p=parameters, dt=dts[i])
+    # Reset diagnostics
+    diagnostics = [RadialCOMDiagnostic(1), ProgressDiagnostic(100), PlotDensityDiagnostic(1000),]
+    prob = SpectralODEProblem(L, N, domain, [u0;;; zero(u0)], [0, tends[i]], p=parameters, dt=dts[i])
+    # Reset diagnostics
+    diagnostics = [RadialCOMDiagnostic(1), ProgressDiagnostic(100), PlotDensityDiagnostic(1000),]
+    # Update output
+    output = Output(prob, 21, diagnostics)
+    # Solve 
+    sol = spectral_solve(prob, MSS3(), output)
+    # Extract velocity
+    velocities[i] = extract_diagnostic(sol.diagnostics[1].data)
+    # Determine max velocity
+    max_velocities[i] = maximum(velocities[i][2, :])
 end
-plot!(data)
 
-data = Array(reshape(reduce(hcat, sol.diagnostics[1].data), size(sol.diagnostics[1].data[1])..., length(sol.diagnostics[1].data)))
-data = Matrix{Number}(data')
-plot(0:0.1:25-0.1, data)
+plot(amplitudes, max_velocities, xaxis=:log, yaxis=:log, marker=:circle)
+savefig("blob velocities log(n).pdf")
