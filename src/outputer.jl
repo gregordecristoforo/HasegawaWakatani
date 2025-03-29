2996b82f8eb719cb69177dc4dfa4cd79167b7180
johannesmorkrid <jomor9370@uit.no>
2025-03-29 18:16:21 +0100

    Parameterized Output with concrete types, fixed some TODOs, commented out recover_fields! as it will be overhalled. Added method to store the cache at any point during the simulation. Overwrote method for write_attribute so that can pass in a SpectralODEProblem. In addition added function for parameter_string for file name. Will make it an option for the Output in a later push.

--- src/outputer.jl
+++ src/outputer.jl
@@ -2,17 +2,18 @@ using HDF5
 using H5Zblosc
 using Dates
 
-mutable struct Output
-    stride::Number
-    diagnostics::AbstractArray
-    u::AbstractArray
-    t::AbstractArray
-    filename::AbstractString
+mutable struct Output{V<:AbstractArray,U<:AbstractArray,T<:AbstractArray,FN<:AbstractString,
+    K<:NamedTuple}
+    stride::Int
+    diagnostics::V
+    u::U
+    t::T
+    filename::FN
     file::HDF5.File
     simulation::HDF5.Group
-    h5_kwargs::NamedTuple #Possibly also called a filter
+    h5_kwargs::K #Possibly also called a filter
 
-    function Output(prob, N_data::Integer, diagnostics::AbstractArray=DEFAULT_DIAGNOSTICS,
+    function Output(prob::SpectralODEProblem, N_data::Integer, diagnostics::AbstractArray=DEFAULT_DIAGNOSTICS,
         filename::AbstractString=basename(tempname()) * ".h5"; h5_kwargs...)
 
         # Calculate number of total samples
@@ -39,7 +40,9 @@ mutable struct Output
             @warn "N_data and stepsize was not compatible, N_data is instead set to N_data = " * "$N_data"
         end
 
-        #TODO check if .h5 and add it if no extension
+        if splitext(filename)[end] == ""
+            filename = splitext(filename)[1]*".h5"
+        end
 
         # Create HDF5 file
         file = h5open(filename, "cw")
@@ -60,10 +63,9 @@ mutable struct Output
         HDF5.set_extent_dims(dset, (N_data,))
 
         # Allocate data for fields
-        # TODO maybe remove u?
         u = Vector{typeof(prob.u0)}(undef, N_data)
         U = copy(prob.u0)
-        prob.recover_fields!(U)
+        #prob.recover_fields!(U)
         u[1] = U
         simulation["fields"][fill(:, ndims(U))..., 1] = u[1]
         t = zeros(N_data)
@@ -71,19 +73,7 @@ mutable struct Output
         simulation["t"][1] = t[1]
 
         # Store attributes
-        # TODO perhaps something like: write_attribute(simulation, prob) and create new method
-        write_attribute(simulation, "dt", prob.dt)
-        write_attribute(simulation, "dx", prob.domain.dx)
-        write_attribute(simulation, "dy", prob.domain.dy)
-        write_attribute(simulation, "L_x", prob.domain.Lx)
-        write_attribute(simulation, "L_y", prob.domain.Ly)
-        write_attribute(simulation, "N_x", prob.domain.Nx)
-        write_attribute(simulation, "N_y", prob.domain.Ny)
-        write_attribute(simulation, "anti_aliased", prob.domain.anti_aliased)
-        write_attribute(simulation, "real_transform", prob.domain.realTransform)
-        for (key, val) in prob.p
-            write_attribute(simulation, key, val)
-        end
+        write_attribute(simulation, prob)
 
         # Allocate data for diagnostics
         for diagnostic in diagnostics
@@ -91,13 +81,14 @@ mutable struct Output
         end
 
         # Create output
-        new(stride, diagnostics, u, t, filename, file, simulation, h5_kwargs)
+        new{typeof(diagnostics),typeof(u),typeof(t),typeof(filename)}(stride, diagnostics,
+            u, t, filename, file, simulation, h5_kwargs)
     end
 end
 
 function handle_output!(output::Output, step::Integer, u::AbstractArray, prob::SpectralODEProblem, t::Number)
     #remove_zonal_modes!(u)
-    
+
     if step % output.stride == 0
         # TODO move logic to spectralSolve
         #if prob.domain.Nx % 2 == 0
@@ -109,7 +100,7 @@ function handle_output!(output::Output, step::Integer, u::AbstractArray, prob::S
         #u[prob.domain.Ny÷2+1, :, :] .= 0
 
         U = real(transform(u, prob.domain.transform.iFT))
-        prob.recover_fields!(U)
+        #prob.recover_fields!(U)
         idx = step ÷ output.stride + 1
 
         output.simulation["fields"][fill(:, ndims(u))..., idx] = U
@@ -132,6 +123,47 @@ function handle_output!(output::Output, step::Integer, u::AbstractArray, prob::S
         error("Breakdown occured at t=$t")
     end
 end
+
+# Perhaps one could look into HDF5 compound types in the future
+function output_cache!(output, cache, step, t)
+    # Create a h5group for the backup cache and get all the attributes
+    cache_group = create_group(output.simulation, "cache backup")
+    keys = fieldnames(typeof(cache))
+
+    # Dump cache
+    for key in keys
+        val = getfield(cache, key)
+        # Do not want to bother with storing Tableau, easy to recover
+        if !isa(val, AbstractTableau)
+            cache_group[string(key)] = val
+        end
+    end
+    # Backup tend
+    cache_group["tend"] = t
+    cache_group["N"] = step
+end
+
+import HDF5.write_attribute
+function write_attribute(simulation::HDF5.Group, prob::SpectralODEProblem)
+    write_attribute(simulation, "dt", prob.dt)
+    write_attribute(simulation, "dx", prob.domain.dx)
+    write_attribute(simulation, "dy", prob.domain.dy)
+    write_attribute(simulation, "L_x", prob.domain.Lx)
+    write_attribute(simulation, "L_y", prob.domain.Ly)
+    write_attribute(simulation, "N_x", prob.domain.Nx)
+    write_attribute(simulation, "N_y", prob.domain.Ny)
+    write_attribute(simulation, "anti_aliased", prob.domain.anti_aliased)
+    write_attribute(simulation, "real_transform", prob.domain.realTransform)
+    for (key, val) in prob.p
+        write_attribute(simulation, key, val)
+    end
+end
+
+function parameter_string(parameters)
+    tmp = [string(key,"=",value) for (key, value) in sort(collect(parameters))] 
+    join(tmp, ", ")
+end
+# -------------------------------------- Old -----------------------------------------------
 
 # Gives a more managle array compared to output.u
 function extract_output(output::Output)
