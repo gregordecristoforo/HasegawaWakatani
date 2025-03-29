61113d8afe551509bc154c8a7118b208885de98c
johannesmorkrid <jomor9370@uit.no>
2025-03-29 17:52:48 +0100

    Made fftutilities typed and added in_place variant. Renamed from transform to spectral_transform, will rename in the other files later. So this push breaks the code

--- src/fftutilities.jl
+++ src/fftutilities.jl
@@ -1,5 +1,7 @@
 using FFTW
-export TransformPlans, FFTPlans, rFFTPlans, multi_fft, multi_ifft
+using LinearAlgebra
+export TransformPlans, FFTPlans, rFFTPlans, spectral_transform, spectral_transform!, 
+multi_fft, multi_ifft,
 
 # Collection of plans for a transform and its inverse
 
@@ -14,6 +16,33 @@ struct rFFTPlans <: TransformPlans
     FT::FFTW.Plan
     iFT::FFTW.Plan
 end
+
+# General transform plan
+function spectral_transform(U::T, p::FFTW.Plan) where {T<:AbstractArray}
+    mapslices(u -> p * u, U, dims=(1, 2))
+end
+
+function spectral_transform(U::T, p::FFTW.Plan) where {T<:Union{Tuple,Vector}}
+    map(u -> p * u, U)
+end
+
+function spectral_transform!(du::DU, u::U, p::FFTW.Plan) where {DU<:AbstractArray,U<:AbstractArray}
+    @assert ndims(du) == ndims(u)
+    idx = ntuple(_ -> :, ndims(p))
+    for i in axes(u, ndims(p)+1)
+        mul!(du[idx..., i], p, u[idx..., i])
+    end
+end
+
+function spectral_transform!(du::DU, u::U, p::FFTW.Plan) where {DU<:Union{Tuple,Vector}, 
+    U<:Union{Tuple,Vector}}
+    for i in eachindex(u)
+        mul!(du[i], p, u[i])
+    end
+end
+
+# ------------------------------------- Old ------------------------------------------------
+# TODO make these typed or remove
 
 # Fourier transform applied to "stacked" fields
 function multi_fft(U::AbstractArray, plans::TransformPlans)
@@ -32,14 +61,4 @@ end
 
 function multi_ifft(U::Union{Tuple,Vector}, plans::TransformPlans)
     map(u -> plans.iFT * u, U)
-end
-
-# TODO possibly rename to spectral_transform
-# General transform plan
-function transform(U::AbstractArray, p::FFTW.Plan)
-    mapslices(u -> p * u, U, dims=(1, 2))
-end
-
-function transform(U::Union{Tuple,Vector}, p::FFTW.Plan)
-    map(u -> p * u, U)
 end
