e10535ae1bb29e7e6f9a99010159e5c829d77262
johannesmorkrid <jomor9370@uit.no>
2025-03-30 12:36:37 +0200

    Major changes to SpectralOperatorCache; it is now typed and the pseudo spectral cache has been realized. The spectral_conv! method is optimized and made parallel using threads. CUDA test reveals the potential of a 25x imporvement to the run time. Optimized the pad! and unpad! functions. All other functions are also typed. quadraticTerm! is now reduced to basicly a wrapper for spectral_conv!, so maybe make it a alias in a future push?

--- src/spectralOperators.jl
+++ src/spectralOperators.jl
@@ -1,192 +1,220 @@
 module SpectralOperators
+using Base.Threads
 include("fftutilities.jl")
 
 export SpectralOperatorCache, FFTPlans, diffX, diffY, diffXX, diffYY, diffusion, solvePhi,
-    poissonBracket, quadraticTerm
+    poissonBracket, quadraticTerm, quadraticTerm!
 
-struct SpectralOperatorCache
+struct SpectralOperatorCache{DX<:AbstractArray,DY<:AbstractArray,DXX<:AbstractArray,
+    DYY<:AbstractArray,L<:AbstractArray,SP<:AbstractArray,P<:AbstractArray,DU<:AbstractArray,
+    T<:TransformPlans}
+
     # Spectral coefficents
-    DiffX::AbstractArray
-    DiffY::AbstractArray
-    DiffXX::AbstractArray
-    DiffYY::AbstractArray
-    Laplacian::AbstractArray
-    invLaplacian::AbstractArray
-    HyperLaplacian::AbstractArray
-    # QT stands for quadratic terms
-    QT::AbstractArray
-    up::AbstractArray
-    vp::AbstractArray
-    QTp::AbstractArray
-    U::AbstractArray
-    V::AbstractArray
-    QTPlans::TransformPlans
-    C::Number
+    DiffX::DX
+    DiffY::DY
+    DiffXX::DXX
+    DiffYY::DYY
+    Laplacian::L
+    invLaplacian::L
+    HyperLaplacian::L
+
+    # Psudo spectral cache
+    padded::Bool
+    up::SP
+    vp::SP
+    U::P
+    V::P
+    qtl::DU
+    qtr::DU
+    C::Float64                    # Coefficient used for correct anti-aliasing normalization
+    QTPlans::T                                               # QT stands for quadratic terms
+
     function SpectralOperatorCache(kx, ky, Nx, Ny; realTransform=true, anti_aliased=true)
-        # Spectral coefficents
+        # Spectral coefficents (TODO All have to be CUDA)
         DiffX = im * kx'
         DiffY = im * ky
         DiffXX = -kx' .^ 2
         DiffYY = -ky .^ 2
         Laplacian = -kx' .^ 2 .- ky .^ 2
         invLaplacian = Laplacian .^ -1
-        HyperLaplacian = Laplacian .^3
+        HyperLaplacian = Laplacian .^ 3
         invLaplacian[1] = 0 # First entry will always be NaN or Inf
-        QT = im * zeros(length(ky), length(kx))
+        qtl = im * zeros(length(ky), length(kx)) # TODO This has to be CUDA
+        qtr = zero(qtl)
 
         if anti_aliased
-            N = Nx > 1 ? ceil(Int, 3 * Nx / 2) : 1
-            M = Ny > 1 ? ceil(Int, 3 * Ny / 2) : 1
+            N = Nx > 1 ? div(3 * Nx, 2, RoundUp) : 1
+            M = Ny > 1 ? div(3 * Ny, 2, RoundUp) : 1
         else
             N, M = Nx, Ny
         end
 
         if realTransform
             m = M % 2 == 0 ? M ÷ 2 + 1 : (M - 1) ÷ 2 + 1
-            QTp = im * zeros(m, N)
-            iFT = plan_irfft(im * QTp, M)
-            FT = plan_rfft(iFT * QTp)
+            spectral_pad = im * zeros(m, N) # TODO This has to be CUDA
+            iFT = plan_irfft(im * spectral_pad, M)
+            FT = plan_rfft(iFT * spectral_pad)
             QT_plans = rFFTPlans(FT, iFT)
         else
-            QTp = im * zeros(M, N)
-            FT = plan_fft(QTp)
-            iFT = plan_ifft(QTp)
+            spectral_pad = im * zeros(M, N)
+            FT = plan_fft(spectral_pad)
+            iFT = plan_ifft(spectral_pad)
             QT_plans = FFTPlans(FT, iFT)
         end
 
-        up = im * zero(QTp)
-        vp = im * zero(QTp)
+        up = zero(spectral_pad)
+        vp = zero(spectral_pad)
 
-        # Count number of dimensions that has more than one dimension along an axis
+        # Calculate correct conversion coefficent
         C = M * N / (Nx * Ny)
-        U = similar(QTp)
-        V = similar(QTp)
-        new(DiffX, DiffY, DiffXX, DiffYY, Laplacian, invLaplacian, HyperLaplacian, QT, up, 
-            vp, QTp, U, V, QT_plans, C)
+        U = iFT * up
+        V = iFT * vp
+        new{typeof(DiffX),typeof(DiffY),typeof(DiffXX),typeof(DiffYY),typeof(Laplacian),
+            typeof(up),typeof(U),typeof(qtr),typeof(QT_plans)
+        }(DiffX, DiffY, DiffXX, DiffYY, Laplacian, invLaplacian, HyperLaplacian,
+            anti_aliased, up, vp, U, V, qtl, qtr, C, QT_plans)
     end
 end
 
 #------------------------- Quadratic terms interface ---------------------------------------
-function quadraticTerm(u, v, SC::SpectralOperatorCache)
-    if length(u) != length(SC.up)
-        pad!(SC.up, u, SC.QTPlans)
-        pad!(SC.vp, v, SC.QTPlans)
-        unpad!(SC.QT, spectral_conv(SC.up, SC.vp, SC.QTPlans), SC.QTPlans)
-        SC.C * SC.QT
-    else
-        spectral_conv(u, v, SC.QTPlans)
+# TODO check if performance loss when doing u::U, v::V
+function quadraticTerm(u::F, v::F, SC::SOC) where {F<:AbstractArray,SOC<:SpectralOperatorCache}
+    spectral_conv!(SC.qtl, u, v, SC)
+end
+
+function quadraticTerm!(out::DF, u::F, v::F, SC::SOC) where {DF<:AbstractArray,
+    F<:AbstractArray,SOC<:SpectralOperatorCache}
+    spectral_conv!(out, u, v, SC)
+end
+
+function spectral_conv!(out::DF, u::F, v::F, SC::SOC) where {DF<:AbstractArray,
+    F<:AbstractArray,SOC<:SpectralOperatorCache}
+    plans = SC.QTPlans
+    # Spawn threads to perform mul! in parallel
+    task_U = Threads.@spawn mul!(SC.U, plans.iFT, SC.padded ? pad!(SC.up, u, plans) : u)
+    task_V = Threads.@spawn mul!(SC.V, plans.iFT, SC.padded ? pad!(SC.vp, v, plans) : v)
+    # Wait for both tasks to finish
+    wait(task_V)
+    wait(task_U)
+
+    @threads for i in eachindex(SC.U)
+        SC.U[i] *= SC.V[i]
+    end
+    mul!(SC.padded ? SC.up : out, plans.FT, SC.U)
+    if SC.padded
+        SC.C * unpad!(out, SC.up, plans)
     end
 end
 
-# function quadraticTerm!(dqt, u, v, SC::SpectralOperatorCache)
-#     if length(u) != length(SC.up)
-#         pad!(SC.up, u, SC.QTPlans)
-#         pad!(SC.vp, v, SC.QTPlans)
-#         unpad!(SC.QT, spectral_conv(SC.up, SC.vp, SC.QTPlans), SC.QTPlans)
-#         SC.C * SC.QT
-#     else
-#         spectral_conv(u, v, SC.QTPlans)
-#     end
-# end
-
+# Kept for legacy 
 function spectral_conv(u_hat, v_hat, plans)
     u = transform(u_hat, plans.iFT)
     v = transform(v_hat, plans.iFT)
     transform(u .* v, plans.FT)
 end
 
-# function spectral_conv!(SC, u_hat, v_hat)
-#     mul!(SC.U, SC.plans.iFT, u_hat)
-#     mul!(SC.V, SC.plans.iFT, v_hat)
-#     SC.QTp .= SC.U.*SC.V
-#     mul!(SC.qtp, SC.plans.FT, sc.QTp)
+# TODO add in a future push
+# function spectral_conv!(qt::DF, u::F, v::F, U::RF, V::RF, up::FP, vp::FP, plans::T) where {
+#     DF<:CuArray,F<:CuArray,RF<:CuArray,FP<:CuArray,T<:TransformPlans}
+#     mul!(U, plans.iFT, padded ? p!(up, u, plans) : u)
+#     mul!(V, plans.iFT, padded ? p!(vp, v, plans) : v)
+#     @. U = U * V
+#     mul!(padded ? up : qt, plans.FT, U)
+#     if padded unpad!(qt, up, plans) end
 # end
 
 # Specialized for 2D arrays
-function pad!(up, u, plan::FFTPlans)
+function pad!(up::DU, u::U, plan::FFTPlans) where {T,DU<:AbstractArray{T},U<:AbstractArray{T}}
+    up .= zero.(up)
     Ny, Nx = size(u)
 
-    Nxl = ceil(Int, Nx / 2)
-    Nxu = floor(Int, Nx / 2)
-    Nyl = ceil(Int, Ny / 2)
-    Nyu = floor(Int, Ny / 2)
+    Nxl = div(Nx, 2, RoundUp)
+    Nxu = div(Nx, 2, RoundDown)
+    Nyl = div(Nx, 2, RoundUp)
+    Nxu = div(Nx, 2, RoundDown)
 
-    up[1:Nyl, 1:Nxl] = u[1:Nyl, 1:Nxl] # Lower left
-    up[1:Nyl, end-Nxu+1:end] = u[1:Nyl, end-Nxu+1:end] # Lower right
-    up[end-Nyu+1:end, 1:Nxl] = u[end-Nyu+1:end, 1:Nxl] # Upper left
-    up[end-Nyu+1:end, end-Nxu+1:end] = u[end-Nyu+1:end, end-Nxu+1:end] # Upper right
-    return
+    @views @inbounds up[1:Nyl, 1:Nxl] .= u[1:Nyl, 1:Nxl] # Lower left
+    @views @inbounds up[1:Nyl, end-Nxu+1:end] .= u[1:Nyl, end-Nxu+1:end] # Lower right
+    @views @inbounds up[end-Nyu+1:end, 1:Nxl] .= u[end-Nyu+1:end, 1:Nxl] # Upper left
+    @views @inbounds up[end-Nyu+1:end, end-Nxu+1:end] .= u[end-Nyu+1:end, end-Nxu+1:end] # Upper right
+    return up
 end
 
-function pad!(up, u, plan::rFFTPlans)
+function pad!(up::DU, u::U, plan::rFFTPlans) where {T,DU<:AbstractArray{T},U<:AbstractArray{T}}
+    up .= zero.(up)
     Ny, Nx = size(u)
 
-    Nxl = ceil(Int, Nx / 2)
-    Nxu = floor(Int, Nx / 2)
+    Nxl = div(Nx, 2, RoundUp)
+    Nxu = div(Nx, 2, RoundDown)
 
-    up[1:Ny, 1:Nxl] = u[1:Ny, 1:Nxl] # Lower left
-    up[1:Ny, end-Nxu+1:end] = u[1:Ny, end-Nxu+1:end] # Lower right
-    return
+    @views @inbounds up[1:Ny, 1:Nxl] .= u[1:Ny, 1:Nxl] # Lower left
+    @views @inbounds up[1:Ny, end-Nxu+1:end] .= u[1:Ny, end-Nxu+1:end] # Lower right
+    return up
 end
 
-function unpad!(u, up, plan::FFTPlans)
+function unpad!(u::DU, up::U, plan::FFTPlans) where {T,DU<:AbstractArray{T},U<:AbstractArray{T}}
     Ny, Nx = size(u)
 
-    Nyl = ceil(Int, Ny / 2)
-    Nyu = floor(Int, Ny / 2)
-    Nxl = ceil(Int, Nx / 2)
-    Nxu = floor(Int, Nx / 2)
+    Nyl = div(Nx, 2, RoundUp)
+    Nyu = div(Nx, 2, RoundDown)
+    Nxl = div(Nx, 2, RoundUp)
+    Nxu = div(Nx, 2, RoundDown)
 
-    u[1:Nyl, 1:Nxl] = up[1:Nyl, 1:Nxl] # Lower left
-    u[1:Nyl, end-Nxu+1:end] = up[1:Nyl, end-Nxu+1:end] # Lower right
-    u[end-Nyu+1:end, 1:Nxl] = up[end-Nyu+1:end, 1:Nxl] # Upper left
-    u[end-Nyu+1:end, end-Nxu+1:end] = up[end-Nyu+1:end, end-Nxu+1:end] # Upper right
-    return
+    @views @inbounds u[1:Nyl, 1:Nxl] .= up[1:Nyl, 1:Nxl] # Lower left
+    @views @inbounds u[1:Nyl, end-Nxu+1:end] .= up[1:Nyl, end-Nxu+1:end] # Lower right
+    @views @inbounds u[end-Nyu+1:end, 1:Nxl] .= up[end-Nyu+1:end, 1:Nxl] # Upper left
+    @views @inbounds u[end-Nyu+1:end, end-Nxu+1:end] .= up[end-Nyu+1:end, end-Nxu+1:end] # Upper right
+    return u
 end
 
-function unpad!(u, up, plan::rFFTPlans)
+function unpad!(u::DU, up::U, plan::rFFTPlans) where {T,DU<:AbstractArray{T},U<:AbstractArray{T}}
     Ny, Nx = size(u)
 
-    Nxl = ceil(Int, Nx / 2)
-    Nxu = floor(Int, Nx / 2)
+    Nxl = div(Nx, 2, RoundUp)
+    Nxu = div(Nx, 2, RoundDown)
 
-    u[1:Ny, 1:Nxl] = up[1:Ny, 1:Nxl] # Lower left
-    u[1:Ny, end-Nxu+1:end] = up[1:Ny, end-Nxu+1:end] # Lower right
-    return
+    @views @inbounds u[1:Ny, 1:Nxl] .= up[1:Ny, 1:Nxl] # Lower left
+    @views @inbounds u[1:Ny, end-Nxu+1:end] .= up[1:Ny, end-Nxu+1:end] # Lower right
+    return u
 end
 
 #-------------------------------- Differentiation ------------------------------------------
 
-function diffX(field, SC::SpectralOperatorCache)
+function diffX(field::F, SC::SOC) where {F<:AbstractArray,SOC<:SpectralOperatorCache}
     SC.DiffX .* field
 end
 
-function diffY(field, SC::SpectralOperatorCache)
+function diffY(field::F, SC::SOC) where {F<:AbstractArray,SOC<:SpectralOperatorCache}
     SC.DiffY .* field
 end
 
-function laplacian(field, SC::SpectralOperatorCache)
-    SC.Laplacian .* field
-end
-
-function diffXX(field, SC::SpectralOperatorCache)
+function diffXX(field::F, SC::SOC) where {F<:AbstractArray,SOC<:SpectralOperatorCache}
     SC.DiffXX .* field
 end
 
-function diffYY(field, SC::SpectralOperatorCache)
+function diffYY(field::F, SC::SOC) where {F<:AbstractArray,SOC<:SpectralOperatorCache}
     SC.DiffYY .* field
 end
 
-function hyper_diffusion(field, SC::SpectralOperatorCache)
-    SC.HyperLaplacian .* field
-end 
-
-function poissonBracket(A, B, SC::SpectralOperatorCache)
-    quadraticTerm(diffX(A, SC), diffY(B, SC), SC) - quadraticTerm(diffY(A, SC), diffX(B, SC), SC)
+function laplacian(field::F, SC::SOC) where {F<:AbstractArray,SOC<:SpectralOperatorCache}
+    SC.Laplacian .* field
 end
 
-function solvePhi(field, SC::SpectralOperatorCache)
+function hyper_diffusion(field::F, SC::SOC) where {F<:AbstractArray,SOC<:SpectralOperatorCache}
+    SC.HyperLaplacian .* field
+end
+
+function solvePhi(field::F, SC::SOC) where {F<:AbstractArray,SOC<:SpectralOperatorCache}
     SC.invLaplacian .* field
+end
+
+# function poissonBracket(A::U, B::V, SC::SOC) where {U<:AbstractArray,SOC<:SpectralOperatorCache}
+#     quadraticTerm(diffX(A, SC), diffY(B, SC), SC) - quadraticTerm(diffY(A, SC), diffX(B, SC), SC)
+# end
+
+function poissonBracket(A::U, B::V, SC::SOC) where {U<:AbstractArray,V<:AbstractArray,
+    SOC<:SpectralOperatorCache}
+    quadraticTerm!(SC.qtl, diffX(A, SC), diffY(B, SC), SC) .-= quadraticTerm!(SC.qtr, diffY(A, SC), diffX(B, SC), SC)
 end
 
 end
