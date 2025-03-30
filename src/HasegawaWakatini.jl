102b4e8649c90175df1efbebaa031864a1551240
johannesmorkrid <jomor9370@uit.no>
2025-03-31 01:27:24 +0200

    Updated which files are done

--- src/HasegawaWakatini.jl
+++ src/HasegawaWakatini.jl
@@ -2,16 +2,16 @@
 #Includes Domain, diagnostics, utilities, spectralSolve, spectralODEProblem and schemes
 #"""
 
-using DotEnv
-DotEnv.load!("src/.env")
+using DotEnv                     # ✓
+DotEnv.load!("src/.env")         # ✓
 using Plots                      # ✓
-include("domain.jl")             # 
+include("domain.jl")             # ✓
 using .Domains                   # ✓
 using LinearAlgebra              # ✓
 using LaTeXStrings               # ✓
 include("spectralODEProblem.jl") # ✓
 include("schemes.jl")            # ✓   
-include("outputer.jl")           # ✓
+include("outputer.jl")           
 include("spectralSolve.jl")      # ✓
-include("diagnostics.jl")        # ✓
+include("diagnostics.jl")        
 include("utilities.jl")          # ✓
