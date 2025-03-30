d0a580df3a2a1f9780aab7e0a6db16eea9789a1b
johannesmorkrid <jomor9370@uit.no>
2025-03-30 12:39:35 +0200

    backup

--- tests/code testing/in-place test.jl
+++ tests/code testing/in-place test.jl
@@ -78,3 +78,85 @@ iFT = plan_irfft(B, 8192)
 # ^^ clear winner
 FFTW.get_num_threads()
 # For single thread that is!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+using BenchmarkTools
+
+c = rand(1024,1024)
+u = rand(1024,1024)
+du = similar(u)
+
+function nodot(du, c, u)
+    du = c*u
+    nothing
+end
+
+function dotmult(du, c, u)
+    du = c.*u
+    nothing
+end
+
+function dotequal(du, c, u)
+    du .= c*u
+    nothing
+end
+
+function dotmacro(du, c, u)
+    @. du = c*u
+    nothing
+end
+
+function twodots(du, c, u)
+    du .= c.*u
+    nothing
+end
+
+function normalmult(c, u)
+    c*u
+end
+
+function normalmultwdot(c, u)
+    c.*u
+end
+
+@time nodot(du, c, u)
+#0.037403 seconds (3 allocations: 8.000 MiB, 26.13% gc time)
+@time dotmult(du, c, u)
+#0.004508 seconds (3 allocations: 8.000 MiB, 59.43% gc time)
+@time dotequal(du,c,u)
+#0.024822 seconds (3 allocations: 8.000 MiB)
+@time twodots(du,c,u)
+#0.001505 seconds
+@time dotmacro(du,c,u)
+#0.001297 seconds
+@time du = normalmult(c,u); nothing
+#0.030025
+@time du = normalmultwdot(c,u); nothing
+#0.004866 seconds (3 allocations: 8.000 MiB, 61.51% gc time)
+@time @. du = normalmult(c,u); nothing
+#0.001483 seconds (1 allocation: 32 bytes)
+@time du .= normalmult(c,u); nothing
+#0.038102 seconds (4 allocations: 8.000 MiB, 21.52% gc time)
+@time du .= normalmultwdot(c,u); nothing
+#0.003212 seconds (4 allocations: 8.000 MiB)
+@time du .= normalmultwdot.(c,u); nothing
+#0.001520 seconds (1 allocation: 32 bytes)
+@time du .= normalmult.(c,u); nothing
+#0.001641 seconds (1 allocation: 32 bytes)
