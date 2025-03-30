962a56e097b925e2fb514314486913b8b5de35b6
johannesmorkrid <jomor9370@uit.no>
2025-03-31 00:46:46 +0200

    Added output_cache! and interupted the error the right way. Added some possible structure to expand upon in later pushes such as resuming simulations

--- src/spectralSolve.jl
+++ src/spectralSolve.jl
@@ -5,15 +5,21 @@ export spectralSolve
 # Assuming for now that dt is fixed
 # If custom outputter is not provided, then resort to default
 # First step is stored during initilization of output
-function spectral_solve(prob::SpectralODEProblem, scheme=MSS3(), output=Output(prob, 100))
+function spectral_solve(prob::SpectralODEProblem, scheme::AbstractODEAlgorithm=MSS3(), 
+                        output::Output=Output(prob, 100); resume=false)
     # Initialize cache
-    cache = get_cache(prob, scheme)
-
-    # Auxilary variables
-    dt = prob.dt
-    t = first(prob.tspan)
-    step = 0
-
+    if resume
+        cache = get_cache(prob, scheme)
+        dt = prob.dt
+        t = first(prob.tspan)
+        step = 0
+    else
+        cache = get_cache(prob, scheme)
+        dt = prob.dt
+        t = first(prob.tspan)
+        step = 0
+    end
+    
     # Calculate number of steps
     total_steps = floor(Int, (last(prob.tspan) - first(prob.tspan)) / dt)
 
@@ -31,11 +37,18 @@ function spectral_solve(prob::SpectralODEProblem, scheme=MSS3(), output=Output(p
             sleep(1e-10000000000000) #To be able to interupt simulation
         end
     catch error
-        # Here store the last 3 steps to be able to resume simulations after interupt
-        println(error.msg)
-        #rethrow(error)
+        # Interupt the error, so that the code does not halt
+        showerror(stdout,error)
     end
+    
+    # Store the cache to be able to resume simulations
+    output_cache!(output, cache, step, t)
+    
     # TODO catch edge case and close file
+    
+    if false #TODO add output.email
+        send_mail("Simulation finished!")
+    end
 
     # Returns output struct
     return output
