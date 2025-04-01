d723425bc0b1627131561be465722cb4d2a0d967
johannesmorkrid <jomor9370@uit.no>
2025-04-01 16:45:30 +0200

    Fixed typo

--- src/diagnostics/parameter_study.jl
+++ src/diagnostics/parameter_study.jl
@@ -1,6 +1,6 @@
 ## ----------------------------------------- Parameter study ----------------------------------------------------------------
 
-function parameterStudy(study::F, values<:V) where {F<:Function,V<:AbstractArray}
+function parameterStudy(study::F, values::V) where {F<:Function,V<:AbstractArray}
     output = similar(values)
     for i in eachindex(values)
         output[i] = study(values[i])
