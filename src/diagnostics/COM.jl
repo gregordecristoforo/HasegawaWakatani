5201d04a59d85e202ba69425c798b38a8c94aca2
johannesmorkrid <jomor9370@uit.no>
2025-04-10 08:35:48 +0200

    Fixed typing of radial_COM

--- src/diagnostics/COM.jl
+++ src/diagnostics/COM.jl
@@ -1,10 +1,10 @@
 # ------------------------------------ COM -------------------------------------------------
 
 # TODO add argument for using quadratures
-function radial_COM(u::U, prob::OP, t::T,p::P) where {U<:AbstractArray,OP<:SpectralODEProblem,
-    T<:Number, P<:AbstractArray}
+function radial_COM(u::U, prob::SOP, t::T, p::P) where {U<:AbstractArray,SOP<:SpectralODEProblem,
+    T<:Number, P<:Dict}
     # 2:end is because the boundaries are periodic and thus should not contribute
-    X_COM = sum(prob.domain.x[2:end]' .* u[2:end, 2:end, 1]) / sum(u[2:end, 2:end, 1])
+    X_COM = @views sum(prob.domain.x[2:end]' .* u[2:end, 2:end, 1]) / sum(u[2:end, 2:end, 1])
 
     # Check that do not divide by zero
     if p["previous_time"] == t
