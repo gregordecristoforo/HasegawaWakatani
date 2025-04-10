fc1508d94ee2dfa8190c34c8afabc029f35ccc02
johannesmorkrid <jomor9370@uit.no>
2025-04-10 14:59:06 +0200

    Corrected benkadda equation

--- tests/Benkadda 1994 cpp/Benkadda 1994 cpp.jl
+++ tests/Benkadda 1994 cpp/Benkadda 1994 cpp.jl
@@ -1,14 +1,14 @@
 ## Run all (alt+enter)
-include(relpath(pwd(), @__DIR__)*"/src/HasegawaWakatini.jl")
+include(relpath(pwd(), @__DIR__) * "/src/HasegawaWakatini.jl")
 
 ## Run Benkadda simulations
 domain = Domain(256, 256, 128, 128, anti_aliased=true)
 # TODO find initial condition
-ic = initial_condition_linear_stability(domain,1e-3)
+ic = initial_condition_linear_stability(domain, 1e-3)
 
 # Linear operator
 function L(u, d, p, t)
-    D_n = p["D"] .* diffusion(u, d) 
+    D_n = p["D"] .* diffusion(u, d)
     D_Ω = p["ν"] .* diffusion(u, d)
     [D_n;;; D_Ω]
 end
@@ -18,11 +18,10 @@ function N(u, d, p, t)
     n = @view u[:, :, 1]
     Ω = @view u[:, :, 2]
     ϕ = solvePhi(Ω, d)
-    
+
     dn = -poissonBracket(ϕ, n, d)
-    dn .-= (p["kappa"] - p["g"]) * diffY(ϕ, d)
     dn .-= diffY(ϕ, d)
-    dn .+= p["σ"] * ϕ
+    dn .+= p["σ"] * n
 
     dΩ = -poissonBracket(ϕ, Ω, d)
     dΩ .-= p["g"] * diffY(n, d)
@@ -41,17 +40,17 @@ parameters = Dict(
 
 t_span = [0, 1000]
 
-prob = SpectralODEProblem(L,N, domain, ic, t_span, p=parameters, dt=1e-3)
+prob = SpectralODEProblem(L, N, domain, ic, t_span, p=parameters, dt=2e-3)
 
 # Diagnostics
 diagnostics = [
     ProgressDiagnostic(1000),
     ProbeDensityDiagnostic((0, 0), N=100),
-    PlotDensityDiagnostic(1000),
-    #RadialFluxDiagnostic(50),
-    #KineticEnergyDiagnostic(50),
-    #PotentialEnergyDiagnostic(50),
-    #EnstropyEnergyDiagnostic(50),
+    #PlotDensityDiagnostic(1000),
+    RadialFluxDiagnostic(50),
+    KineticEnergyDiagnostic(50),
+    PotentialEnergyDiagnostic(50),
+    EnstropyEnergyDiagnostic(50),
     #GetLogModeDiagnostic(50, :ky),
     #CFLDiagnostic(50),
     #RadialPotentialEnergySpectraDiagnostic(50),
@@ -62,20 +61,20 @@ diagnostics = [
 
 # Output
 cd("tests/Benkadda 1994 cpp")
-output = Output(prob, 1001, diagnostics, "output/benkadda april tenth.h5", simulation_name=:parameters)
+output = Output(prob, 1001, diagnostics, "output/benkadda april tenth corrected.h5", simulation_name=:parameters)
 
 FFTW.set_num_threads(16)
 
 ## Solve and plot
 sol = spectral_solve(prob, MSS3(), output, resume=true)
 
-data = sol.simulation["fields"][:,:,:,:]
+data = sol.simulation["fields"][:, :, :, :]
 t = sol.simulation["t"][:]
 default(legend=false)
 anim = @animate for i in axes(data, 4)
-    heatmap(data[:, :, 1, i], aspect_ratio=:equal, xaxis=L"x", yaxis=L"y", title=L"n(t="*"$(round(t[i], digits=0)))")
+    heatmap(data[:, :, 1, i], aspect_ratio=:equal, xaxis=L"x", yaxis=L"y", title=L"n(t=" * "$(round(t[i], digits=0)))")
 end
-gif(anim, "benkadda.gif", fps = 20)
+gif(anim, "benkadda.gif", fps=20)
 
-send_mail("Benkadda simulation finnished!", attachment="benkadda.gif")
+send_mail("Corrected benkadda simulation finnished!", attachment="benkadda.gif")
 close(output.file)
