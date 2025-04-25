51c8468561edd0c8b54014fc131dabaa81eaac3a
johannesmorkrid <jomor9370@uit.no>
2025-04-26 00:26:14 +0200

    Removed and added some lines, this file is not really used and probably should be removed at some point

--- tests/code testing/Linear stability test.jl
+++ tests/code testing/Linear stability test.jl
@@ -1,17 +1,9 @@
 ## Run all (alt+enter)
 include(relpath(pwd(), @__DIR__)*"/src/HasegawaWakatini.jl")
 
-## Run scheme test for Burgers equation
-#domain = Domain(512, 512, 200, 100, anti_aliased=false)
-domain = Domain(128, 128, 500, 500, anti_aliased=false)
-#domain = Domain(1024, 1024, 50, 50, anti_aliased=false)
-#u0 = gaussian.(domain.x', domain.y, A=0.1, B=1, l=10)
-#u0 = all_modes_with_random_phase(domain, 1e-6)
-#phi0 = all_modes_with_random_phase(domain, 1e-6)
-#Omega0 = domain.transform.iFT*diffusion(domain.transform.FT*phi0,domain)
-
-ic = initial_condition_linear_stability(domain)
-#ic = [u0;;; zero(u0)]
+## Run alternative linear stability test
+domain = Domain(256, 256, 100, 100, anti_aliased=true)
+ic = initial_condition_linear_stability(domain, 1e-6)
 
 # Linear operator (May not be static actually)
 function L(u, d, p, t)
@@ -46,7 +38,7 @@ parameters = Dict(
 )
 
 # Time interval
-t_span = [0, 3600]
+t_span = [0, 500]
 
 # The problem
 prob = SpectralODEProblem(L, N, domain, ic, t_span, p=parameters, dt=1e-3)
@@ -68,16 +60,6 @@ output = Output(prob, 1001, diagnostics) #progressDiagnostic
 
 ## Solve and plot
 sol = spectral_solve(prob, MSS3(), output)
-
-## Recreate Garcia et al. plots
-display(heatmap(sol.u[5][:, :, 1], levels=10, aspect_ratio=:equal))
-display(heatmap(sol.u[10][:, :, 1], levels=10, aspect_ratio=:equal))
-display(heatmap(sol.u[15][:, :, 1], levels=10, aspect_ratio=:equal))
-display(heatmap(sol.u[end][:, :, 1], levels=10, aspect_ratio=:equal))
-display(heatmap(sol.u[5][:, :, 2], levels=10, aspect_ratio=:equal, color=:jet))
-display(heatmap(sol.u[10][:, :, 2], levels=10, aspect_ratio=:equal, color=:jet))
-display(heatmap(sol.u[15][:, :, 2], levels=10, aspect_ratio=:equal, color=:jet))
-display(heatmap(sol.u[end][:, :, 2], levels=10, aspect_ratio=:equal, color=:jet))
 
 ## Save data
 using JLD
@@ -102,31 +84,9 @@ plot(domain.x, n0[1, :] + 1e4 * sol.u[end][1, :, 1])
 
 plot(log.(abs.(data_hat[14, 14, 1, :])))
 
-# Check if can get growth rate from taking difference between two last data points
-gamma = log.(abs.(data_hat[:, :, 1, end])) .- log.(abs.(data_hat[:, :, 1, end-1]))
-plot(fftshift(domain.kx)[end-62:end], fftshift(real(diag(gamma)))[end-62:end], xaxis=:log)
-ylims!(0, 0.003)
+cd("tests/code testing")
+fid = h5open("output/linear-stability test.h5", "r")
+simulation = fid[keys(fid)[1]]
 
-fftshift(domain.kx)[end-62]
-
-
-gamma = sol.diagnostics[3].data[end][:,1] - sol.diagnostics[3].data[end-1][:,1]
-plot(gamma, xaxis=:log)
-
-for i in eachindex(sol.diagnostics[3].data)
-    display(plot(sol.diagnostics[3].data[i][:,1]))
-end
-
-mapreduce(permutedims, hcat, sol.diagnostics[3].data)
-
-
-sol.diagnostics[3].data[1]
-
-Array(reshape(reduce(hcat, output.u), size(output.u[1])..., length(output.u)))
-
-a = reshape(reduce(hcat, sol.diagnostics[3].data), (64,2,36001))
-
-plot(domain.kx[2:64], a[2:64,1,end]-a[2:64,1,end-1], xaxis=:log)
-
-gamma = a[1:64,1,end]-a[1:64,1,end-1]
-gamma[4]
+simulation["Log mode diagnostic/data"][:,:,:]
+simulation["Log mode diagnostic/t"][:]
