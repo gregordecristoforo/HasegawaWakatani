c179b1edc6f4b3d537d1e4604829c0f314acb4c9
JohannesMorkrid <jomor9370@uit.no>
2025-11-04 11:46:32 +0100

    Cleaned up diagnostics a bit

--- src/diagnostics/diagnostics.jl
+++ src/diagnostics/diagnostics.jl
@@ -27,113 +27,61 @@ struct Diagnostic{N<:AbstractString,M<:Function,L<:AbstractString,A<:Tuple,
     end
 end
 
-function initialize_diagnostic!(diagnostic::D, simulation::S, h5_kwargs::K, u0::T, prob::SOP,
-    t0::Number; store_hdf::Bool=true, store_locally::Bool=true) where
-{D<:Diagnostic,S<:Union{HDF5.Group,Nothing},T<:AbstractArray,SOP<:SpectralODEProblem,K<:NamedTuple}
+"""
+    (diagnostic::Diagnostic)(state, prob, time)
 
-    # Calculate total number of steps
-    N_steps = floor(Int, (last(prob.tspan) - first(prob.tspan)) / prob.dt)
-
-    # If user did not specify 
-    if diagnostic.sample_step == -1
-        #TODO implement some logic here later
-        diagnostic.sample_step = 1
-        # N_samples = floor(Int, 0.1 * N_steps)
-
-        # if N_samples > N_steps
-        #     N_samples = N_steps + 1
-        #     @warn "N_samples and stepsize was not compatible, N_samples is instead set to N_samples = " * "$N_samples"
-        # end
-
-        # # Calculate number of evolution steps between samples
-        # fieldStep = floor(Int, N_steps / (N_samples - 1))
-    end
-
-    if diagnostic.sample_step > N_steps
-        diagnostic.sample_step = N_steps
-        @warn "The sample step was larger than the number of steps and has been set to sample_step = $N_steps"
-    end
-
-    # Compute timeline
-
-    # Take diagnostic of initial field (id = initial diagnostic)
-    if diagnostic.assumes_spectral_field
-        id = diagnostic.method(prob.u0_hat, prob, first(prob.tspan), diagnostic.args...; diagnostic.kwargs...)
-    else
-        id = diagnostic.method(u0, prob, first(prob.tspan), diagnostic.args...; diagnostic.kwargs...)
-    end
-
-    if diagnostic.stores_data
-        # Calculate number of samples with rounded sampling rate
-        N = floor(Int, N_steps / diagnostic.sample_step) + 1
-
-        if N_steps % diagnostic.sample_step != 0
-            @warn "($(diagnostic.name)) Note, there is a $(diagnostic.sample_step + N_steps%diagnostic.sample_step) sample step at the end"
-        end
-        if store_hdf
-            if !haskey(simulation, diagnostic.name)
-                # Create group
-                diagnostic.h5group = create_group(simulation, diagnostic.name)
-
-                # Create dataset for fields and time
-                ## Datatype and shape is not so trivial here..., will have to think about it tomorrow
-                dset = create_dataset(simulation[diagnostic.name], "data", datatype(eltype(id)), (size(id)..., typemax(Int64)),
-                    chunk=(size(id)..., 1); h5_kwargs...)
-                HDF5.set_extent_dims(dset, (size(id)..., N))
-                dset = create_dataset(simulation[diagnostic.name], "t", datatype(eltype(id)), (typemax(Int64),),
-                    chunk=(1,); h5_kwargs...)
-                HDF5.set_extent_dims(dset, (N,))
-
-                # Add labels
-                create_attribute(diagnostic.h5group, "labels", diagnostic.labels)
-
-                # Store initial diagnostic
-                diagnostic.h5group["data"][fill(:, ndims(id))..., 1] = id
-                diagnostic.h5group["t"][1] = first(prob.tspan)
-            else
-                diagnostic.h5group = open_group(simulation, diagnostic.name)
-                # Extend size of arrays
-                # Open dataset
-                dset = open_dataset(diagnostic.h5group, "data")
-                HDF5.set_extent_dims(dset, (size(id)..., N))
-                dset = open_dataset(diagnostic.h5group, "t")
-                HDF5.set_extent_dims(dset, (N,))
-            end
-        end
-
-        if store_locally
-            # Allocate arrays
-            diagnostic.data = [zero(id) for _ in 1:N] #Vector{typeof(id)}(undef, N)
-            diagnostic.t = zeros(N)
-
-            # Store intial diagnostic
-            if isa(id, AbstractArray)
-                diagnostic.data[1] .= id
-            else
-                diagnostic.data[1] = copy(id)
-            end
-            diagnostic.t[1] = first(prob.tspan)
-        end
-    end
+  Apply diagnostic `method` with the right `args` and `kwargs` stored in the `Diagnostic`.
+"""
+@inline function (diagnostic::Diagnostic)(state, prob, time)
+    diagnostic.method(state, prob, time, diagnostic.args...; diagnostic.kwargs...)
 end
 
-function perform_diagnostic!(diagnostic::D, step::Integer, u::U, prob::SOP, t::N;
-    store_hdf::Bool=true, store_locally::Bool=true) where {D<:Diagnostic,U<:AbstractArray,
-    SOP<:SpectralODEProblem,N<:Number}
-    # u might be real or complex depending on previous handle_output and diagnostic.assumes_spectral_field
+# Take diagnostic of initial field (id = initial diagnostic)
+# if diagnostic.assumes_spectral_state
+#   id = diagnostic.method(prob.u0_hat, prob, first(prob.tspan), diagnostic.args...;
+#                          diagnostic.kwargs...)
+#   else
+#       id = diagnostic.method(u0, prob, first(prob.tspan), diagnostic.args...;
+#                              diagnostic.kwargs...)
+#   end
 
-    # Perform diagnostic # TODO make diagnostic.args..., u, prob, t
-    data = diagnostic.method(u, prob, t, diagnostic.args...; diagnostic.kwargs...)
+# -------------------------------- Building Of Diagnostics ---------------------------------
 
-    if !isnothing(data)
-        # Calculate index
-        idx = step ÷ diagnostic.sample_step + 1
-
-        store_hdf ? write_data(diagnostic, idx, data, t) : nothing
-
-        store_locally ? write_local_data(diagnostic, idx, data, t) : nothing
-    end
+function build_diagnostic(method::Function; kwargs...)
+    build_diagnostic(Val(Symbol(method)); kwargs...)
 end
+
+# function perform_diagnostic!(diagnostic::D, step::Integer, u::U, prob::SOP, t::N;
+#                              store_hdf::Bool=true,
+#                              store_locally::Bool=true) where {D<:Diagnostic,
+#                                                               U<:AbstractArray,
+#                                                               SOP<:SpectralODEProblem,
+#                                                               N<:Number}
+#     # u might be real or complex depending on previous handle_output and diagnostic.assumes_spectral_state
+
+#     # Perform diagnostic # TODO make diagnostic.args..., u, prob, t
+#     data = diagnostic.method(u, prob, t, diagnostic.args...; diagnostic.kwargs...)
+
+#     if !isnothing(data)
+#         # Calculate index
+#         idx = step ÷ diagnostic.sample_step + 1
+
+#         store_hdf ? write_data(diagnostic, idx, data, t) : nothing
+
+#         store_locally ? write_local_data(diagnostic, idx, data, t) : nothing
+#     end
+# end
+
+"""
+"""
+function Base.show(io::IO, m::MIME"text/plain", diagnostic::Diagnostic)
+    print(io, diagnostic.name, ": (spectral=", diagnostic.assumes_spectral_state,
+          ", stores_data=", diagnostic.stores_data, ")")
+    length(diagnostic.args) != 0 ? print(io, ", args=", diagnostic.args) : nothing
+    length(diagnostic.kwargs) != 0 ? print(io, ", kwargs=", diagnostic.kwargs) : nothing
+end
+
+# TODO REMOVE OR REWRITE THE THREE METHODS BELOW
 
 # TODO perhaps make more like write_state
 function write_data(diagnostic, idx, data, t)
@@ -152,29 +100,22 @@ function write_local_data(diagnostic::Diagnostic, idx, data, t)
     diagnostic.t[idx] = t
 end
 
-function Base.show(io::IO, m::MIME"text/plain", diagnostic::Diagnostic)
-    print(io, diagnostic.name, " (stride: ", diagnostic.sample_step, ", spectral=",
-        diagnostic.assumes_spectral_field, ", stores_data=", diagnostic.stores_data, ")")
-    length(diagnostic.args) != 0 ? print(io, ", args=", diagnostic.args) : nothing
-    length(diagnostic.kwargs) != 0 ? print(io, ", kwargs=", diagnostic.kwargs) : nothing
-end
-
 """
 """
 function sample_diagnostic!(output, diagnostic, step::Integer, u, prob, t)
     # Check if diagnostic assumes physical field and transform if not yet done
-    if !diagnostic.assumes_spectral_field && !output.transformed
+    if !diagnostic.assumes_spectral_state && !output.transformed
         # Transform state
         transform_state!(output, u, get_bwd(prob.domain))
     end
 
     # Passes the logic onto perform_diagnostic! to do diagnostic and store data
-    if diagnostic.assumes_spectral_field
-        perform_diagnostic!(diagnostic, step, u, prob, t,
-            store_hdf=output.store_hdf, store_locally=output.store_locally)
+    if diagnostic.assumes_spectral_state
+        perform_diagnostic!(diagnostic, step, u, prob, t;
+                            store_hdf=output.store_hdf, store_locally=output.store_locally)
     else
-        perform_diagnostic!(diagnostic, step, output.U_buffer, prob, t,
-            store_hdf=output.store_hdf, store_locally=output.store_locally)
+        perform_diagnostic!(diagnostic, step, output.U_buffer, prob, t;
+                            store_hdf=output.store_hdf, store_locally=output.store_locally)
     end
 end
 
@@ -220,7 +161,7 @@ end
 
 """
 
-Allows for single method()
+  Allows for single method()
 """
 function parse_diagnostic_expr(expr)
     if expr isa Symbol
