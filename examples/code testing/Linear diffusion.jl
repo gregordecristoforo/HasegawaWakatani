b7df396344d84713b2e435cf39f473fb2d0eefdb
JohannesMorkrid <jomor9370@uit.no>
2025-11-13 09:59:47 +0100

    Updated diffusion tests, plots are currently outdated

--- examples/code testing/Linear diffusion.jl
+++ examples/code testing/Linear diffusion.jl
@@ -1,47 +1,49 @@
 ## Run all (alt+enter)
-include(relpath(pwd(), @__DIR__) * "/src/HasegawaWakatini.jl")
+using HasegawaWakatani
 
 ## Run scheme test
-domain = Domain(256, 256, 50, 50, dealiased=false)
+domain = Domain(256, 256; Lx=50, Ly=50)
 u0 = initial_condition(gaussian, domain)
 
 # Diffusion 
-function L(u, d, p, t)
-    p["nu"] * laplacian(u, d)
-end
-
-function N(u, d, p, t)
-    zero(u)
-end
+Linear(du, u, operators, p, t) = du .= p.ν * operators.laplacian(u)
+NonLinear(du, u, operators, p, t) = du .= zero(u)
 
 # Parameters
-parameters = Dict(
-    "nu" => 0.5
-)
+parameters = (ν=0.5,)
 
-t_span = [0, 2]
+tspan = [0.0, 2.0]
 
-prob = SpectralODEProblem(L, N, u0, domain, t_span, p=parameters, dt=1e-3)
+diagnostics = @diagnostics [progress(; stride=10)]
 
-cd(relpath(@__DIR__, pwd()))
-output = Output(prob, 21, [ProgressDiagnostic(10)], "output/linear diffusion.h5")#, simulation_name=":parameters")
+prob = SpectralODEProblem(Linear, NonLinear, u0, domain, tspan; p=parameters, dt=1e-3,
+                          diagnostics=diagnostics, operators=:all)
+
+output_file_name = joinpath(@__DIR__, "output", "linear diffusion.h5")
+output = Output(prob; filename=output_file_name, simulation_name=:parameters)
 
 ## Solve and plot
-#sol = spectral_solve(prob, MSS2(), output)
+sol = spectral_solve(prob, MSS3(), output)
 
+## TODO update the plots below
 #norm(sol.u[end]-HeatEquationAnalyticalSolution2(u0, domain, parameters, last(sol.t)))
 
 ## Time convergence test
 timesteps = [1e-1, 1e-2, 1e-3, 1e-4, 1e-5]
-_, convergence1 = test_timestep_convergence(prob, HeatEquationAnalyticalSolution2, timesteps, MSS1())
-_, convergence2 = test_timestep_convergence(prob, HeatEquationAnalyticalSolution2, timesteps, MSS2())
-_, convergence3 = test_timestep_convergence(prob, HeatEquationAnalyticalSolution2, timesteps, MSS3())
-plot(timesteps, convergence1, xaxis=:log, yaxis=:log, label="MSS1")
-plot!(timesteps, convergence2, xaxis=:log, yaxis=:log, label="MSS2", color="dark green")
-plot!(timesteps, convergence3, xaxis=:log, yaxis=:log, label="MSS3", color="orange")
-plot!(timesteps, 0.5 * timesteps .^ 3, linestyle=:dash, label=L"\frac{1}{2}dt^3")
-plot!(timesteps, 0.5 * timesteps .^ 2, linestyle=:dash, label=L"\frac{1}{2}dt^2", xlabel="dt",
-    ylabel=L"||U-u_a||", title="Timestep convergence, Lin-Diffusion (N =$(domain.Nx))", xticks=timesteps)
+_, convergence1 = test_timestep_convergence(prob, HeatEquationAnalyticalSolution2,
+                                            timesteps, MSS1())
+_, convergence2 = test_timestep_convergence(prob, HeatEquationAnalyticalSolution2,
+                                            timesteps, MSS2())
+_, convergence3 = test_timestep_convergence(prob, HeatEquationAnalyticalSolution2,
+                                            timesteps, MSS3())
+plot(timesteps, convergence1; xaxis=:log, yaxis=:log, label="MSS1")
+plot!(timesteps, convergence2; xaxis=:log, yaxis=:log, label="MSS2", color="dark green")
+plot!(timesteps, convergence3; xaxis=:log, yaxis=:log, label="MSS3", color="orange")
+plot!(timesteps, 0.5 * timesteps .^ 3; linestyle=:dash, label=L"\frac{1}{2}dt^3")
+plot!(timesteps, 0.5 * timesteps .^ 2; linestyle=:dash, label=L"\frac{1}{2}dt^2",
+      xlabel="dt",
+      ylabel=L"||U-u_a||", title="Timestep convergence, Lin-Diffusion (N =$(domain.Nx))",
+      xticks=timesteps)
 savefig("figures/Timestep convergence, Lin-Diffusion (N =$(domain.Nx)).pdf")
 
 using JLD
@@ -56,16 +58,24 @@ end
 
 ## Resolution convergence test
 resolutions = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]
-_, convergence1 = test_resolution_convergence(prob, gaussian, HeatEquationAnalyticalSolution2, resolutions, MSS1())
-_, convergence2 = test_resolution_convergence(prob, gaussian, HeatEquationAnalyticalSolution2, resolutions, MSS2())
-_, convergence3 = test_resolution_convergence(prob, gaussian, HeatEquationAnalyticalSolution2, resolutions, MSS3())
+_, convergence1 = test_resolution_convergence(prob, gaussian,
+                                              HeatEquationAnalyticalSolution2, resolutions,
+                                              MSS1())
+_, convergence2 = test_resolution_convergence(prob, gaussian,
+                                              HeatEquationAnalyticalSolution2, resolutions,
+                                              MSS2())
+_, convergence3 = test_resolution_convergence(prob, gaussian,
+                                              HeatEquationAnalyticalSolution2, resolutions,
+                                              MSS3())
 
-plot(resolutions, convergence1, xaxis=:log2, yaxis=:log, label="MSS1")
-plot!(resolutions, convergence2, xaxis=:log2, yaxis=:log, label="MSS2", color="dark green")
-plot!(resolutions, convergence3, xaxis=:log2, yaxis=:log, label="MSS3", color="orange")
-plot!(resolutions[1:end-4], 0.5 * exp.(-0.5 * resolutions)[1:end-4], label=L"\frac{1}{2}\exp\left(-\frac{N}{2}\right)", linestyle=:dash,
-    xaxis=:log2, yaxis=:log, xticks=resolutions, xlabel=L"N_x \wedge N_y",
-    ylabel=L"||U-u_a||/N_xN_y", title="Resolution convergence, Lin-Diffusion (dt=$(prob.dt))")
+plot(resolutions, convergence1; xaxis=:log2, yaxis=:log, label="MSS1")
+plot!(resolutions, convergence2; xaxis=:log2, yaxis=:log, label="MSS2", color="dark green")
+plot!(resolutions, convergence3; xaxis=:log2, yaxis=:log, label="MSS3", color="orange")
+plot!(resolutions[1:end-4], 0.5 * exp.(-0.5 * resolutions)[1:end-4];
+      label=L"\frac{1}{2}\exp\left(-\frac{N}{2}\right)", linestyle=:dash,
+      xaxis=:log2, yaxis=:log, xticks=resolutions, xlabel=L"N_x \wedge N_y",
+      ylabel=L"||U-u_a||/N_xN_y",
+      title="Resolution convergence, Lin-Diffusion (dt=$(prob.dt))")
 savefig("figures/Resolution convergence, Lin-Diffusion (dt=$(prob.dt)).pdf")
 
 jldopen("output/linear diffusion resolution test.jld", "w") do file
--- examples/code testing/Non-linear diffusion.jl
+++ examples/code testing/Non-linear diffusion.jl
@@ -1,52 +1,59 @@
 ## Run all (alt+enter)
-include(relpath(pwd(), @__DIR__) * "/src/HasegawaWakatini.jl")
+using HasegawaWakatani
 
 ## Run scheme test
-domain = Domain(256, 256, 50, 50, dealiased=true)
+domain = Domain(256, 256; Lx=50, Ly=50)
 u0 = initial_condition(log_gaussian, domain)
 
 # Diffusion 
-function L(u, d, p, t)
-    p["nu"] * laplacian(u, d)
-end
+Linear(du, u, operators, p, t) = du .= p.ν * operators.laplacian(u)
 
-function N(u, d, p, t)
-    p["nu"] * (quadratic_term(diff_x(u, d), diff_x(u, d), d) .+ quadratic_term(diff_y(u, d), diff_y(u, d), d))
+function NonLinear(du, u, operators, p, t)
+    @unpack diff_x, diff_y, quadratic_term = operators
+    @unpack ν = p
+    du .= ν * (quadratic_term(diff_x(u), diff_x(u)) .+ quadratic_term(diff_y(u), diff_y(u)))
 end
 
 # Parameters
-parameters = Dict(
-    "nu" => 0.5
-)
+parameters = (ν=0.5,)
 
-t_span = [0, 2]
+tspan = [0.0, 2.0]
 
-prob = SpectralODEProblem(L, N, u0, domain, t_span, p=parameters, dt=0.001)
+diagnostics = @diagnostics [progress(; stride=10)]
+
+prob = SpectralODEProblem(Linear, NonLinear, u0, domain, tspan; p=parameters, dt=0.001,
+                          diagnostics=diagnostics, operators=:all)
 
 function inverse_transform!(U::V) where {V<:AbstractArray}
     @views U[:, :, 1] .= exp.(U[:, :, 1]) .- 1
 end
 
-cd(relpath(@__DIR__, pwd()))
-output = Output(prob, 21, [ProgressDiagnostic(10)], "output/non-linear diffusion.h5", physical_transform=inverse_transform!)
+output_file_name = joinpath(@__DIR__, "output", "non-linear diffusion.h5")
+output = Output(prob; filename=output_file_name, physical_transform=inverse_transform!)
 
-## Solve and plot
+# Solve and plot
 sol = spectral_solve(prob, MSS3(), output)
 
+## TODO update the plots below
 ## Time convergence test
 timesteps = [1e-1, 1e-2, 1e-3, 1e-4, 1e-5]
-_, convergence1 = test_timestep_convergence(prob, HeatEquationAnalyticalSolution2, timesteps,
-    MSS1(), physical_transform=inverse_transform!)
-_, convergence2 = test_timestep_convergence(prob, HeatEquationAnalyticalSolution2, timesteps,
-    MSS2(), physical_transform=inverse_transform!)
-_, convergence3 = test_timestep_convergence(prob, HeatEquationAnalyticalSolution2, timesteps,
-    MSS3(), physical_transform=inverse_transform!)
-plot(timesteps, convergence1, xaxis=:log, yaxis=:log, label="MSS1")
-plot!(timesteps, convergence2, xaxis=:log, yaxis=:log, label="MSS2", color="dark green")
-plot!(timesteps, convergence3, xaxis=:log, yaxis=:log, label="MSS3", color="orange")
-plot!(timesteps, 0.5 * timesteps .^ 3, linestyle=:dash, label=L"\frac{1}{2}dt^3")
-plot!(timesteps, 0.5 * timesteps .^ 2, linestyle=:dash, label=L"\frac{1}{2}dt^2", xlabel="dt",
-    ylabel=L"||U-u_a||", title="Timestep convergence, Nonlin-Diffusion (N =$(domain.Nx))", xticks=timesteps)
+_, convergence1 = test_timestep_convergence(prob, HeatEquationAnalyticalSolution2,
+                                            timesteps,
+                                            MSS1(); physical_transform=inverse_transform!)
+_, convergence2 = test_timestep_convergence(prob, HeatEquationAnalyticalSolution2,
+                                            timesteps,
+                                            MSS2(); physical_transform=inverse_transform!)
+_, convergence3 = test_timestep_convergence(prob, HeatEquationAnalyticalSolution2,
+                                            timesteps,
+                                            MSS3(); physical_transform=inverse_transform!)
+plot(timesteps, convergence1; xaxis=:log, yaxis=:log, label="MSS1")
+plot!(timesteps, convergence2; xaxis=:log, yaxis=:log, label="MSS2", color="dark green")
+plot!(timesteps, convergence3; xaxis=:log, yaxis=:log, label="MSS3", color="orange")
+plot!(timesteps, 0.5 * timesteps .^ 3; linestyle=:dash, label=L"\frac{1}{2}dt^3")
+plot!(timesteps, 0.5 * timesteps .^ 2; linestyle=:dash, label=L"\frac{1}{2}dt^2",
+      xlabel="dt",
+      ylabel=L"||U-u_a||", title="Timestep convergence, Nonlin-Diffusion (N =$(domain.Nx))",
+      xticks=timesteps)
 savefig("figures/Timestep convergence, Nonlin-Diffusion (N =$(domain.Nx)).pdf")
 
 using JLD
@@ -61,19 +68,27 @@ end
 
 ## Resolution convergence test
 resolutions = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]
-_, convergence1 = test_resolution_convergence(prob, log_gaussian, HeatEquationAnalyticalSolution2,
-    resolutions, MSS1(), physical_transform=inverse_transform!)
-_, convergence2 = test_resolution_convergence(prob, log_gaussian, HeatEquationAnalyticalSolution2,
-    resolutions, MSS2(), physical_transform=inverse_transform!)
-_, convergence3 = test_resolution_convergence(prob, log_gaussian, HeatEquationAnalyticalSolution2,
-    resolutions, MSS3(), physical_transform=inverse_transform!)
+_, convergence1 = test_resolution_convergence(prob, log_gaussian,
+                                              HeatEquationAnalyticalSolution2,
+                                              resolutions, MSS1();
+                                              physical_transform=inverse_transform!)
+_, convergence2 = test_resolution_convergence(prob, log_gaussian,
+                                              HeatEquationAnalyticalSolution2,
+                                              resolutions, MSS2();
+                                              physical_transform=inverse_transform!)
+_, convergence3 = test_resolution_convergence(prob, log_gaussian,
+                                              HeatEquationAnalyticalSolution2,
+                                              resolutions, MSS3();
+                                              physical_transform=inverse_transform!)
 
-plot(resolutions, convergence1, xaxis=:log2, yaxis=:log, label="MSS1")
-plot!(resolutions, convergence2, xaxis=:log2, yaxis=:log, label="MSS2", color="dark green")
-plot!(resolutions, convergence3, xaxis=:log2, yaxis=:log, label="MSS3", color="orange")
-plot!(resolutions[1:end-4], 0.5 * exp.(-0.5 * resolutions)[1:end-4], label=L"\frac{1}{2}\exp\left(-\frac{N}{2}\right)", linestyle=:dash,
-    xaxis=:log2, yaxis=:log, xticks=resolutions, xlabel=L"N_x \wedge N_y",
-    ylabel=L"||U-u_a||/N_xN_y", title="Resolution convergence, Nonlin-Diffusion (dt=$(prob.dt))")
+plot(resolutions, convergence1; xaxis=:log2, yaxis=:log, label="MSS1")
+plot!(resolutions, convergence2; xaxis=:log2, yaxis=:log, label="MSS2", color="dark green")
+plot!(resolutions, convergence3; xaxis=:log2, yaxis=:log, label="MSS3", color="orange")
+plot!(resolutions[1:end-4], 0.5 * exp.(-0.5 * resolutions)[1:end-4];
+      label=L"\frac{1}{2}\exp\left(-\frac{N}{2}\right)", linestyle=:dash,
+      xaxis=:log2, yaxis=:log, xticks=resolutions, xlabel=L"N_x \wedge N_y",
+      ylabel=L"||U-u_a||/N_xN_y",
+      title="Resolution convergence, Nonlin-Diffusion (dt=$(prob.dt))")
 savefig("figures/Resolution convergence, Nonlin-Diffusion (dt=$(prob.dt)).pdf")
 
 jldopen("output/non-linear diffusion resolution test.jld", "w") do file
